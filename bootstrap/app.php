46a2b5cb5d24537f5e107a6b4b406af16be19d19
Gajos <thegajos@gmail.com>
2022-09-15 22:41:18 +0200

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -65,6 +65,7 @@ $app->configure('openweathermap');
 $app->configure('slack');
 $app->configure('auth');
 $app->configure('jwt');
+$app->configure('cors');
 
 /*
 |--------------------------------------------------------------------------
@@ -77,9 +78,9 @@ $app->configure('jwt');
 |
 */
 
-// $app->middleware([
-//     App\Http\Middleware\ExampleMiddleware::class
-// ]);
+$app->middleware([
+    Fruitcake\Cors\HandleCors::class,
+]);
 
 $app->routeMiddleware([
     'auth' => App\Http\Middleware\Authenticate::class,
@@ -103,6 +104,7 @@ $app->register(App\Providers\AuthServiceProvider::class);
 
 $app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
 $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
+$app->register(Fruitcake\Cors\CorsServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,9 @@ $router->get('/', function () use ($router) {
 
 Route::group(['prefix' => 'api'], function ($router) {
     $router->post('login', 'AuthController@login');
-    $router->post('logout', 'AuthController@logout');
-    $router->get('refresh', 'AuthController@refresh');
+    $router->post('refresh', 'AuthController@refresh');
     $router->get('me', 'AuthController@me');
+    $router->post('logout', 'AuthController@logout');
 
     $router->get('furnace/temperature', 'FurnaceTemperatureController@show');
     $router->post('furnace/temperature', 'FurnaceTemperatureController@store');
